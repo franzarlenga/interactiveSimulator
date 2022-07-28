eb27e0c0dc72a6b6233b61c480e8661310c0c494
Francisco Zarlenga <franzarlenga6@gmail.com>
2022-07-28 18:35:48 -0300

    agregando objetos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-let name = prompt('Enter your name:')
+/*let name = prompt('Enter your name:'),
 let parcial1 = parseInt(prompt('Enter the first note:'))
-let parcial2 = parseInt(prompt('Enter the second note:'))
-let parcial3 = parseInt(prompt('Enter the third note:'))
+let parcial2 = parseInt(prompt('Enter the second note:')) 
+let parcial3 = parseInt(prompt('Enter the third note:')) */
 
-let noteList = [parcial1, parcial2, parcial3]
+const user = {
+    name:prompt('Enter your name:'),
+    parcial1: parseInt(prompt('Enter the first note:')),
+    parcial2: parseInt(prompt('Enter the second note:')),
+    parcial3: parseInt(prompt('Enter the third note:'))
+}
+
+let noteList = [user.parcial1, user.parcial2, user.parcial3]
 
 function noteCounter(noteList){
     let noteCounter = 0
@@ -23,8 +30,13 @@ function averageC(counter){
 let averageFinal = averageC(counter);
 
 if(averageFinal >= 6) {
-    alert ('Congratulations ' + name + '!' + ' you passed the Math exam with an average of ' + averageFinal)
+    alert ('Congratulations ' + user.name + '!' + ' you passed the Math exam with an average of ' + averageFinal)
     }else if(averageFinal < 6){
-    alert (name + ', ' + 'see you in summer clases, your average is ' + averageFinal + ' :(')
+    alert (user.name + ', ' + 'see you in summer clases, your average is ' + averageFinal + ' :(')
     }
 
+/* 
+
+
+
+*/
